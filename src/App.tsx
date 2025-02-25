024424d134632c0c8fef012b39a015eabbb3ce58
Brendan McGuire <brendan@bren.app>
2025-02-24 21:06:33 -0600

    Display details about each month

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,23 @@ type Month =
 
 type ItemsByDate = Record<Month, Record<number, FeedItem[]>>;
 
+const DAY_COUNTS: Record<Month, number> = {
+  January: 31,
+  February: 29,
+  March: 31,
+  April: 30,
+  May: 31,
+  June: 30,
+  July: 31,
+  August: 31,
+  September: 30,
+  October: 31,
+  November: 30,
+  December: 31,
+};
+
+const TOTAL_DAYS = Object.values(DAY_COUNTS).reduce((a, b) => a + b, 0);
+
 export const App: React.FC = () => {
   // RSS
   const [rss, setRss] = useState<string>(initial);
@@ -38,17 +55,19 @@ export const App: React.FC = () => {
 
   const onBlur = useCallback(() => {}, []);
 
-  const { data, isFetching } = useQuery({
+  const { data, isFetching, isError, error } = useQuery({
     queryKey: ["rss", rss],
     queryFn: async () => {
       if (!isValid) {
-        return;
+        return null;
       }
 
       const response = await fetch(rss);
       const xml = await response.text();
       return parseFeed(xml);
     },
+    enabled: isValid,
+    refetchOnWindowFocus: false,
   });
 
   const publishedDates = useMemo(() => {
@@ -80,7 +99,7 @@ export const App: React.FC = () => {
       const month = timestamp.toLocaleString("default", {
         month: "long",
       }) as Month;
-      const date = timestamp.getDate();
+      const date = timestamp.getDate() - 1;
 
       if (!counts[month][date]) {
         counts[month][date] = [];
@@ -90,6 +109,32 @@ export const App: React.FC = () => {
     }
 
     return counts;
+  }, [data]);
+
+  const totalDays = useMemo(() => {
+    let count = 0;
+
+    for (const month in publishedDates) {
+      for (const day in publishedDates[month as Month]) {
+        if (publishedDates[month as Month][day].length > 0) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  }, [publishedDates]);
+
+  const oldest = useMemo(() => {
+    let oldest = Date.now();
+    for (const item of data?.items ?? []) {
+      const timestamp = (item.published ?? item.updated)?.getTime();
+      if (timestamp && timestamp < oldest) {
+        oldest = timestamp;
+      }
+    }
+
+    return new Date(oldest);
   }, [data]);
 
   return (
@@ -108,64 +153,77 @@ export const App: React.FC = () => {
         />
       </header>
       <Spinner show={rss.length > 0 && isFetching} />
+      {isError && !isFetching ? <p className="error">{error.message}</p> : null}
       {data ? (
         <>
           <PodcastPreview feed={data} />
+          <section className="callout-section">
+            <p>
+              Since {oldest.toLocaleDateString()}, <strong>{data.title}</strong>{" "}
+              has published <strong>{data.items.length}</strong> episodes. Those
+              episodes have spanned {totalDays} days out of {TOTAL_DAYS} total
+              on the calendar ({((100 * totalDays) / TOTAL_DAYS).toFixed(2)}%).
+            </p>
+          </section>
           <main>
             <Month
               title="January"
-              days={31}
+              days={DAY_COUNTS.January}
               highlighted={publishedDates["January"]}
             />
             <Month
               title="February"
-              days={29}
+              days={DAY_COUNTS.February}
               highlighted={publishedDates["February"]}
             />
             <Month
               title="March"
-              days={31}
+              days={DAY_COUNTS.March}
               highlighted={publishedDates["March"]}
             />
             <Month
               title="April"
-              days={30}
+              days={DAY_COUNTS.April}
               highlighted={publishedDates["April"]}
             />
-            <Month title="May" days={31} highlighted={publishedDates["May"]} />
+            <Month
+              title="May"
+              days={DAY_COUNTS.May}
+              highlighted={publishedDates["May"]}
+            />
             <Month
               title="June"
-              days={30}
+              days={DAY_COUNTS.June}
               highlighted={publishedDates["June"]}
             />
             <Month
               title="July"
-              days={31}
+              days={DAY_COUNTS.July}
               highlighted={publishedDates["July"]}
             />
             <Month
               title="August"
-              days={31}
+              days={DAY_COUNTS.August}
               highlighted={publishedDates["August"]}
             />
             <Month
               title="September"
-              days={30}
+              days={DAY_COUNTS.September}
               highlighted={publishedDates["September"]}
             />
             <Month
               title="October"
-              days={31}
+              days={DAY_COUNTS.October}
               highlighted={publishedDates["October"]}
             />
             <Month
               title="November"
-              days={30}
+              days={DAY_COUNTS.November}
               highlighted={publishedDates["November"]}
             />
             <Month
               title="December"
-              days={31}
+              days={DAY_COUNTS.December}
               highlighted={publishedDates["December"]}
             />
           </main>
--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -1,4 +1,5 @@
 import { FeedItem } from "@rowanmanning/feed-parser/lib/feed/item/base";
+import { useMemo, useState } from "react";
 
 export type MonthProps = {
   title: string;
@@ -7,20 +8,64 @@ export type MonthProps = {
 };
 
 export const Month: React.FC<MonthProps> = ({ title, days, highlighted }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const totalDays = useMemo(() => {
+    let count = 0;
+
+    for (const key in highlighted) {
+      if (Object.hasOwn(highlighted, key)) {
+        count += highlighted[key].length > 0 ? 1 : 0;
+      }
+    }
+
+    return count;
+  }, [highlighted]);
+
   return (
     <div className="month">
-      <h3 className="title">{title}</h3>
-      {Array.from({ length: days }, (_, i) => (
-        <div
-          key={i}
-          className="day"
-          data-highlighted={
-            Object.hasOwn(highlighted, i) && highlighted[i].length > 0
-          }
-        >
-          {i + 1}
+      <h3 className="title">
+        <span>
+          {title} ({totalDays}/{days} days)
+        </span>
+        <button className="icon-button" onClick={() => setIsOpen((o) => !o)}>
+          <span>More</span>
+        </button>
+      </h3>
+      {isOpen ? (
+        Object.entries(highlighted).map(([day, items]) => (
+          <div key={day}>
+            <h4>
+              {title} {+day + 1}
+            </h4>
+            <ul>
+              {items.map((item) => (
+                <li key={item.url}>
+                  <a href={item.url ?? "#"} target="_blank" rel="noreferrer">
+                    {item.title}
+                  </a>
+                  &nbsp;
+                  <em>({item.published?.toLocaleDateString()})</em>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))
+      ) : (
+        <div className="day-container">
+          {Array.from({ length: days }, (_, i) => (
+            <div
+              key={i}
+              className="day"
+              data-highlighted={
+                Object.hasOwn(highlighted, i) && highlighted[i].length > 0
+              }
+            >
+              {i + 1}
+            </div>
+          ))}
         </div>
-      ))}
+      )}
     </div>
   );
 };
